d7cff5c195d586215c138e985a4b06513690eb7e
m.kuznetsov <m.kuznetsov@norsi-trans.ru>
2024-11-28 14:59:44 +0300

    Fixes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Производственная система</title>
     <link href="/favicoSokol.png" rel="icon">
-    <!-- <script type="module" src="/build/assets/app-BEgpbGFS.js"></script>
+    <script type="module" src="/build/assets/app-CCrEE0Pg.js"></script>
     <link rel="stylesheet" href="/build/assets/app-w0NttVeA.css">
     <link rel="stylesheet" href="/build/assets/app-DoJqK5qg.css">
-    <link rel="manifest" href="/build/manifest.json"> -->
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    <link rel="manifest" href="/build/manifest.json">
+    <!-- @vite(['resources/js/app.js', 'resources/css/app.css']) -->
 </head>
 <body>
     <div id="app"></div>
