33ecdb1bf6264afaa64ea5650ed1b7895d15fe4b
m.kuznetsov <m.kuznetsov@norsi-trans.ru>
2025-01-27 11:14:40 +0300

    fefefe

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Производственная система</title>
     <link href="/favicoSokol.png" rel="icon">
-    <script type="module" src="/build/assets/app-C5enre4n.js"></script>
+    <script type="module" src="/build/assets/app-D47iJSTd.js"></script>
     <link rel="stylesheet" href="/build/assets/app-w0NttVeA.css">
     <link rel="stylesheet" href="/build/assets/app-DoJqK5qg.css">
     <link rel="manifest" href="/build/manifest.json">
