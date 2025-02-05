5fc1197cee87d1a251655a91b07197d8314ddf25
m.kuznetsov <m.kuznetsov@norsi-trans.ru>
2025-02-05 11:34:39 +0300

    efef

--- resources/js/vueapp/boards.vue
+++ resources/js/vueapp/boards.vue
@@ -575,7 +575,7 @@ export default {
 }
 </script>
 <template>
-    <div style="height: fit-content; margin-left: 1vw;gap:15pc;display: flex;">
+    <div style="height: fit-content; margin-left: 1vw;gap:15px;display: flex;">
         <Button type="dashed" @click="() => showList = !showList">{{ !showList ? 'Показать список рабочих' : 'Скрыть'
             }}</Button>
         <Button type="default"@click="print_graph">
