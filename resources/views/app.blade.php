e84ae63dff83bb77773ee0ee82a4ff36db3fafdb
gasonger0 <gasonger0@gmail.com>
2025-04-13 22:55:41 +0300

    vdvdv

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Производственная система</title>
     <link href="/favicoSokol.png" rel="icon">
-    <script type="module" src="/build/assets/app-D44m92YR.css"></script>
+    <script type="module" src="/build/assets/app-gWZspzBR.js"></script>
     <link rel="stylesheet" href="/build/assets/app-dVVLog6c.css">
-    <link rel="stylesheet" href="/build/assets/app-gWZspzBR.js">
+    <link rel="stylesheet" href="/build/assets/app-D44m92YR.css">
     <link rel="manifest" href="/build/manifest.json">   
     <!-- @vite(['resources/js/app.js', 'resources/css/app.css']) -->
 </head>
